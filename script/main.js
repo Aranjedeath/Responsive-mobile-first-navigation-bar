e19c7ca4d37a94210f609dab16704034503c635f
Brandon Dail <Kierkegaurd@gmail.com>
2014-08-03 15:02:09 -0500

    A general purpose navigator bar
    This project uses jQuery and CSS to form a responsive sliding
    navigation bar that works on both desktop and mobile devices.

---
+++ script/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+if($(window).width() > "740px"){
+	$("#m_nav_container_").show();
+}
+else{
+	$("#m_nav_container_").hide();
+}
+
+$(window).resize(toggle_nav_container());
+
+});
+
+
+
+var toggle_nav_container = function () {
+
+	
+
+	var 	$toggleButton = $('#toggle_m_nav');
+			$navContainer = $('#m_nav_container');
+			$menuButton = $('#m_nav_menu')
+			$menuButtonBars = $('.m_nav_ham');
+			$wrapper = $('#wrapper');
+	// hide the menu by default, show the menu button
+	$menuButton.show();
+
+
+	// toggle the container on click of button (can be remapped to $menuButton)
+
+	$toggleButton.on("click", function(){
+
+		// declare a local variable for the window width
+		var $viewportWidth = $(window).width();
+
+		// if statement to determine whether the nav container is already toggled or not
+
+		if($navContainer.is(':hidden'))
+		{
+			$navContainer.slideDown('slow');
+			$menuButtonBars.removeClass('button_closed')
+			$menuButtonBars.addClass('button_open')
+			$wrapper.removeClass('closed_wrapper')
+			$wrapper.addClass("open_wrapper")
+		}
+		else
+		{
+			$navContainer.slideUp('slow');
+			$menuButtonBars.removeClass('button_open')
+			$menuButtonBars.addClass('button_closed')
+			$wrapper.removeClass('open_wrapper')
+			$wrapper.addClass("closed_wra")		}
+	});
+
+
+
+
+
+}
+
+
+
+
+
+
