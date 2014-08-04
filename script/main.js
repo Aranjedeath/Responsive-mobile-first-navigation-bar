9dd413bf74df7a8e863864d352cdaf4ee7a21cbb
Brandon Dail <Kierkegaurd@gmail.com>
2014-08-04 16:15:16 -0500

    v1.1.0
    *Improved m_nav_container positioning and closing/opening animations
    while scrolled
    
    *Implemented a gotoByScroll() function that takes the href value of the
    n_nav_item anchors and slides to the corresponding div

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 toggle_nav_container();
-toggle_nav_zindex();
+gotoByScroll();
 
 
 });
@@ -29,21 +29,54 @@ var toggle_nav_container = function () {
 
 		if($navContainer.is(':hidden'))
 		{	
-			$wrapper.removeClass('closed_wrapper')
-			$wrapper.addClass("open_wrapper")
-			$navContainer.slideDown(1500).addClass('container_open').delay(2000).css("z-index", "1");
-			$menuButtonBars.removeClass('button_closed')
-			$menuButtonBars.addClass('button_open')
+			$wrapper.removeClass('closed_wrapper');
+			$wrapper.addClass("open_wrapper");
+			$navContainer.slideDown(200).addClass('container_open').css("z-index", "2");
+			// $(window).scrollTop(0);
+			$menuButtonBars.removeClass('button_closed');
+			$menuButtonBars.addClass('button_open');
+			$("#m_ham_1").addClass("m_nav_ham_1_open");
+			$("#m_ham_2").addClass("m_nav_ham_2_open");
+			$("#m_ham_3").addClass("m_nav_ham_3_open");
 
 		}
 		else
 		{
-			$navContainer.css("z-index", "0").removeClass('container_open').slideUp(1500)
+			$navContainer.css("z-index", "0").removeClass('container_open').slideUp(200)
 			$menuButtonBars.removeClass('button_open')
 			$menuButtonBars.addClass('button_closed')
 			$wrapper.removeClass('open_wrapper')
-			$wrapper.addClass("closed_wrapper")		}
+			$wrapper.addClass("closed_wrapper")
+			$("#m_ham_1").removeClass("m_nav_ham_1_open");
+			$("#m_ham_2").removeClass("m_nav_ham_2_open");
+			$("#m_ham_3").removeClass("m_nav_ham_3_open");
+
+		}
 	});
+
+
+
+}
+
+
+// Function that takes the href value of links in the navbar and then scrolls 
+//the div on the page whose ID matches said value. This only works if you use 
+//a consistent naming scheme for the navbar anchors and div IDs
+
+var gotoByScroll = function (){
+
+	$(".m_nav_item a").on("click", function(e) {
+
+		e.preventDefault();
+		// var $divID =$(this).attr("href");
+		// var $scrollToDiv = "$(" + "'" + $divID + "'" +")";
+		
+		$('html,body').animate({
+   scrollTop: $($(this).attr("href")).offset().top - 50
+}, "slow");
+
+	});
+		
 
 
 
